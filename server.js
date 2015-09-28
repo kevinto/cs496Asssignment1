fe8cbd33de923bd851843c189aef219e1ffb99d8
kevinto@github.com <kevinto@github.com>
2015-09-27 18:09:32 -0700

    added new node project structure from express generator

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ app.set('port', (process.env.PORT || 8080));
 
 // Write main homepage route
 app.get('/', function (request, response) {
-  response.send('Hello World!');
+  var currentUtcTime = Date.now();
+  var displayUtcTime = 'Current Utc Time: ' + currentUtcTime + '<br/>';
+
+  var currentDateTime = new Date();
+  var displayTime = "Current Local Date Time at the server: " +
+                    (currentDateTime.getMonth() + 1) + "/" +
+                    currentDateTime.getDate() + "/" +
+                    currentDateTime.getFullYear() + " " +
+                    currentDateTime.getHours() + ":" +
+                    currentDateTime.getMinutes() + ":" +
+                    currentDateTime.getSeconds() + '\r\n';
+  response.send(displayUtcTime + displayTime);
 })
 
 app.listen(app.get('port'), function() {
